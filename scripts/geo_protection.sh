55dcb2bd459d378b89d6f70e506a892988b276b7
JoDerGraf <jonas@jograf.de>
2023-01-05 19:17:24 +0100

    Update geo_protection.sh

--- scripts/geo_protection.sh
+++ scripts/geo_protection.sh
@@ -4,11 +4,11 @@ ipset destroy country_whitelist
 ipset -N -! country_whitelist hash:net maxelem 100000
 
 # get country ip blocks
-country_list="https://raw.githubusercontent.com/herrbischoff/country-ip-blocks/master/ipv4/"
+country_list="https://github.com/herrbischoff/country-ip-blocks/blob/master/ipv4/at.cidr"
 
 # add whitelisted countries to an ipset
 # countries from https://gist.github.com/oqo0/47a185af30c966a362dbdfebf3771400
-for ip in $(curl -L $country_list/{de,us,uk,fr,es,ca,au,it}.cidr);
+for ip in $(curl -L $country_list/{de,uk,fr,es,ca,au,ch,it}.cidr);
     do ipset -A country_whitelist $ip
 done
 
