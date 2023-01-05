316df02e94e75f26b42bd9349e62cb20624ab409
JoDerGraf <jonas@jograf.de>
2023-01-05 19:31:34 +0100

    Update experimental_protection.sh

--- scripts/experimental_protection.sh
+++ scripts/experimental_protection.sh
@@ -21,10 +21,10 @@ iptables -A PREROUTING -p tcp --tcp-flags ALL SYN,RST,ACK,FIN,URG -j DROP
 # block spoofed packets
 iptables -A PREROUTING -s 224.0.0.0/3 -j DROP
 iptables -A PREROUTING -s 169.254.0.0/16 -j DROP
-iptables -A PREROUTING -s 172.16.0.0/12 -j DROP
+#iptables -A PREROUTING -s 172.16.0.0/12 -j DROP
 iptables -A PREROUTING -s 192.0.2.0/24 -j DROP
 iptables -A PREROUTING -s 192.168.0.0/16 -j DROP
 iptables -A PREROUTING -s 10.0.0.0/8 -j DROP
-iptables -A PREROUTING -s 0.0.0.0/8 -j DROP
+#iptables -A PREROUTING -s 0.0.0.0/8 -j DROP
 iptables -A PREROUTING -s 240.0.0.0/5 -j DROP
-iptables -A PREROUTING -s 127.0.0.0/8 ! -i lo -j DROP
+#iptables -A PREROUTING -s 127.0.0.0/8 ! -i lo -j DROP
